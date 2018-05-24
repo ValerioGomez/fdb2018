de46621c17257252b0ba167033e16e10e9006416
Valerio Gomez <valesitoo@gmail.com>
2018-05-23 19:19:19 -0500

    Initial commit

---
+++ FireBirdConnect/src/firebirdconnect/FireBirdConnect.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firebirdconnect;
+import java.sql.*;
+/**
+ *
+ * @author vales
+ */
+public class FireBirdConnect {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String databaseURL="jdbc:firebirdsql:localhost/3050:c:/fdb2018/MIBASEUNAPUNO.FDB";
+        String usuariodb="sysdba";
+        String passworddb="masterkey";
+        String driverName="org.firebirdsql.jdbc.FBDriver";
+        Connection conexion;
+        Statement consulta;
+        ResultSet resultado;
+        try{
+           Class.forName(driverName);
+           conexion=DriverManager.getConnection(databaseURL,usuariodb,passworddb);
+           consulta=conexion.createStatement();
+           String consultaSQL="select*from emp";
+           resultado=consulta.executeQuery(consultaSQL);
+           
+           while(resultado.next()){
+               String ename=resultado.getString("ename");
+               String job=resultado.getString("job");
+               System.out.println(ename+"___"+job);
+           }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+}
